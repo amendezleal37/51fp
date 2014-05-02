f25717102d07fac4bbd7822ad6520e0dad4d22e0
oscarxia <oscarxia1@gmail.com>
2014-05-02 19:25:35 -0400

    Dijkstra’s working!
    Dijkstra’s working!

--- dijkstra.py
+++ dijkstra.py
@@ -18,62 +18,39 @@ http://en.wikipedia.org/wiki/Dijkstra's_algorithm
 import yardgraph
 yardmap = yardgraph.graph
 
-# start and end are the id's of the two nodes
-# can be changed by the user
-start = 4
-end = 1
-
-#def dijkstra(start, end):
-  
-# initializes unvisited to include all nodes
-unvisited = range(yardmap.ID)
-
-# initializes list of known shortest distances from start
-dist = [float('inf')] * yardmap.ID
-
-# initializes the list of previous nodes for each node to keep track of paths
-prev = ['null'] * yardmap.ID
+def dijkstra(start):  
+    # initializes unvisited to include all nodes
+    unvisited = range(yardmap.ID)
     
-# start node has 0 distance to itself
-dist[start] = 0
-
-# start node removed from unvisited
-unvisited.remove(start)
-
-current = start
-
-while (end in unvisited):
-    for neighbor in yardmap.list[current][2]:
-        if (neighbor in unvisited):
-            new_dist = dist[current] + yardmap.distance(current, neighbor)
-            if (new_dist < dist[neighbor]):
-                dist[neighbor] = new_dist
-                prev[neighbor] = current
+    # initializes list of known shortest distances from start
+    dist = [float('inf')] * yardmap.ID
     
-    if (current in unvisited):
-        unvisited.remove(current)
+    # initializes the list of previous nodes for each node to keep track of paths
+    prev = ['null'] * yardmap.ID
+        
+    # start node has 0 distance to itself
+    dist[start] = 0
     
-    if len(unvisited) != 0:
-        current = unvisited[0]
-        for i in unvisited:
-            if dist[i] < current:
-                current = i
+    # start node removed from unvisited
+    unvisited.remove(start)
+    
+    current = start
+    
+    while len(unvisited) != 0:
+        for neighbor in yardmap.list[current][2]:
+            if (neighbor in unvisited):
+                new_dist = dist[current] + yardmap.distance(current, neighbor)
+                if (new_dist < dist[neighbor]):
+                    dist[neighbor] = new_dist
+                    prev[neighbor] = current
+        
+        if (current in unvisited):
+            unvisited.remove(current)
+        
+        if len(unvisited) != 0:
+            current = unvisited[0]
+            for i in unvisited:
+                if dist[i] < dist[current]:
+                    current = i
+    return prev
 
-"""
-def update_neighbor(current, neighbor):
-    if (neighbor in unvisited):
-        new_dist = dist[current] + map.distance(current, neighbor)
-        if (new_dist < dist[neighbor]) or (dist[neighbor]==-1):
-            dist[neighbor] = new_dist
-            prev[neighbor] = current
-
-while (end in unvisited):
-    for neighbor in map.list[current][2]:
-        update_neighbors ...
-    if (current in unvisited):
-        unvisited.remove(current)
-    for i in map.list[current][2]:
-        if i in unvisited:
-            current = i
-            break
-"""
