28d940dfcf6f80da686a4ddd6061ac7941131db6
Zahra Mahmood <zahramahmood16@gmail.com>
2014-05-02 15:01:26 -0400

    khkhjghg

---
+++ BellmanFord.py
@@ -0,0 +1,16 @@
+# Created on Thur Apr 24 2014
+# 
+# Author: Zahra Mahmood
+
+import yardgraph
+map = yardgraph.graph
+# template for node is (pos, [edges])
+
+print map.list
+
+# def BellmanFord(vertices, edges, vertex_source):
+# 	weight = []
+# 	predecessor = []
+# 	for v in vertices:
+# 		if v == vertex_source:
+
